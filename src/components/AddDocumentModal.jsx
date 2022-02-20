380a472b4e09c9c19e1586b12066071e12c25d64
12jyo <jyotiss1312@gmail.com>
2022-02-20 21:03:37 +0530

    added event handlers

--- src/components/AddDocumentModal.jsx
+++ src/components/AddDocumentModal.jsx
@@ -11,24 +11,40 @@ function AddDocumentModal({ onClose }) {
     onClose();
   };
 
+  const [collection, setCollection] = useState("");
+  const [document, setDocument] = useState({});
+
+  const handleJsonInput = event => {
+    console.log(event);
+  };
+
+  const save = () => {
+    console.log(collection);
+  };
+
   return (
     <Modal show={show} fullscreen={true} onHide={hide}>
       <Modal.Header closeButton>
         <Modal.Title>Modal</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <input className="text-input" type="text" name="gg" id="gg" />
-        <div className="container ">
-          <JSONInput
-            placeholder={{}} // data to display
-            theme="light_mitsuketa_tribute"
-            locale={locale}
-            colors={{
-              string: "#DAA520" // overrides theme colors with whatever color value you want
-            }}
-            height="550px"
-          />
+        <input className="text-input" type="text" name="gg" id="gg" onChange={(e) => {
+          setCollection(e.target.value)
+        }}/>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlTextarea1">
+            Basic textarea
+            </label>
+            <textarea
+          onChange={setDocument}
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="5"
+            />
         </div>
+        <button onClick={() => {
+          save();
+        }}>Save</button>
       </Modal.Body>
     </Modal>
   );
